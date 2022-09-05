5bfd5edfa53a8476cdbeca04dfeb8813b069743e
qinzhihao-conghua <1043419697@qq.com>
2022-09-05 21:21:29 +0800

    修改

--- config/index.js
+++ config/index.js
@@ -17,10 +17,10 @@ export const config = {
       // 想要发送的人的名字
       name: "大漂亮",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      // id: "oQl-d5xVI2bcYjNx3AqAwT7tARJU",
+      id: "oQl-d5xVI2bcYjNx3AqAwT7tARJU",
       // 你想对他发送的模板消息的模板ID
       // useTemplateId: "v8fRZJYxQmrgxvFpN-VVcYZSFbsoKgOQoTo1Tmor67g",
-      id: "oQl-d532IT8uhXUUoWeMbeyEiOTg",
+      // id: "oQl-d532IT8uhXUUoWeMbeyEiOTg",
       useTemplateId: "1gbkWorCWlzj87sg0NqxGVWMNsgqVs8iY-vPW8ublb0",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://blog.csdn.net/moqiuqin?type=blog"
